1d49e4b155903da104e5d196e31199caa99aecbc
Diego Fernando Aparicio Castro <dapariciocastro13@gmail.com>
2025-08-05 21:17:59 -0500

    Update index1.js

--- TrabajosInstructorAlex/Portafolio/Informacion/index1.js
+++ TrabajosInstructorAlex/Portafolio/Informacion/index1.js
@@ -9,34 +9,36 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const lugar = lugares.find(t => t.id === id);
 
+  document.body.style.background=`radial-gradient(${lugar.categoria.colorPrimario}, ${lugar.categoria.colorSecundario})`
+
   document.getElementById("ContenedorGeneral").innerHTML = `
 
 
   <div class="Encabezado">
 
-  <div class="NombreT"><h1>${lugar.nombre}</h1></div>
+  <div class="NombreT"><h1 style="color: ${lugar.categoria.colorSecundario};">${lugar.nombre}</h1></div>
   
   <div class="Imagen"><img src="${lugar.url_imagen}" alt="${lugar.nombre}"></div>
 
   </div>
 
-    <div class="TodaInformacion">
+    <div class="TodaInformacion" >
 
     <div class="DatosBasicos">
-      <h1>Datos Básicos</h1>
+      <h1 style="color: ${lugar.categoria.colorSecundario};">Datos Básicos</h1>
       <h3>Pais: ${lugar.pais}</h3>
       <h4>Ciudad: ${lugar.ciudad}</h4>
-      <h5>Descripcion: ${lugar.descripcion}</h5>
+      <h5 style="color: ${lugar.categoria.colorSecundario};">Descripcion: ${lugar.descripcion}</h5>
     </div>
 
     <div class="Ubicacion">
-      <h1>Coordenadas</h1>
+      <h1 style="color: ${lugar.categoria.colorSecundario};">Coordenadas</h1>
       <p>Latitud: ${lugar.coordenadas.latitud}</p>
       <p>Longitud: ${lugar.coordenadas.longitud}</p>
     </div>
 
     <div class="DatosInteresantes">
-      <h1>Datos Interesantes</h1>
+      <h1 style="color: ${lugar.categoria.colorSecundario};">Datos Interesantes</h1>
       <ul>
         <li>${lugar.datosInteresantes[0].titulo}: ${lugar.datosInteresantes[0].valor}</li>
         <li>${lugar.datosInteresantes[1].titulo}: ${lugar.datosInteresantes[1].valor}</li>
@@ -45,7 +47,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     </div>
 
     <div class="ActividadesRecomendadas">
-      <h1>Actividades Recomendadas</h1>
+      <h1 style="color: ${lugar.categoria.colorSecundario};">Actividades Recomendadas</h1>
       <ul>
         <li>${lugar.actividadesRecomendadas[0]}</li>
         <li>${lugar.actividadesRecomendadas[1]}</li>
@@ -59,4 +61,5 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     </div>
   `;
+
 });
